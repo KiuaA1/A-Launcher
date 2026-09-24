2fbdd29232518b0bb3315d897cc34339171eedf7
KiuaA1 <aakiua4@gmail.com>
2026-09-24 17:20:03 +0300

    phase1: restore engine module exports

--- engine/core/src/lib.rs
+++ engine/core/src/lib.rs
@@ -1,9 +1,13 @@
 pub mod cache;
+pub mod download;
 pub mod error;
 pub mod fs;
+pub mod instance;
 pub mod launch;
 pub mod manifest;
 pub mod mojang;
+pub mod process;
 pub mod resolver;
+pub mod runtime;
 
 pub const ENGINE_VERSION: &str = env!("CARGO_PKG_VERSION");
