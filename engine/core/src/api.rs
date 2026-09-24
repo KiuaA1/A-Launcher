32b13f318106d16c00e21bc784e992bfe84dc44b
KiuaA1 <aakiua4@gmail.com>
2026-09-24 19:21:28 +0300

    engine: use Rust 2024 native ABI attribute

--- engine/core/src/api.rs
+++ engine/core/src/api.rs
@@ -57,7 +57,7 @@ impl From<crate::LaunchState> for LaunchStateDto {
 pub const API_VERSION: u32 = 1;
 
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn native_engine_ready() -> i32 {
     1
 }
