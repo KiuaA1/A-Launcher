1feb9aa187f1f80834d19becde9460a0349656f7
KiuaA1 <aakiua4@gmail.com>
2026-09-24 17:51:49 +0300

    phase1: connect launcher engine to instances

--- engine/core/src/engine.rs
+++ engine/core/src/engine.rs
@@ -1,17 +1,17 @@
 use std::path::PathBuf;
-use crate::{error::EngineError,fs::StorageLayout,LaunchEvent,LaunchState,mojang::MojangResolver,resolver::{Resolution,TargetPlatform},download::{DownloadRequest,DownloadTransport,prepare_download},classpath::{Classpath,build_classpath},native::extract_native_jar,plan::LaunchPreparation,runtime::RuntimeManager,manifest::VersionJson,arguments::LaunchContext,launch::LaunchPlan,process::{DefaultProcessManager,ManagedProcess,ProcessManager,ProcessEvent};
+use crate::{error::EngineError,fs::StorageLayout,instances::InstanceManager,instance::Instance,LaunchEvent,LaunchState,mojang::MojangResolver,resolver::{Resolution,TargetPlatform},download::{DownloadRequest,DownloadTransport,prepare_download},classpath::{Classpath,build_classpath},native::extract_native_jar,plan::LaunchPreparation,runtime::RuntimeManager,manifest::VersionJson,arguments::LaunchContext,launch::LaunchPlan,process::{DefaultProcessManager,ManagedProcess,ProcessManager,ProcessEvent};
 
 #[derive(Debug,Clone)]
-pub struct LauncherEngine { pub storage:StorageLayout }
+pub struct LauncherEngine { pub storage:StorageLayout, pub instances:InstanceManager }
 
 impl LauncherEngine {
  pub fn new(root:impl Into<PathBuf>)->Result<Self,EngineError>{
   let storage=StorageLayout::new(root);
   storage.ensure_dirs().map_err(|e|EngineError::RuntimeUnavailable(format!("initialize storage: {e}")))?;
-  Ok(Self{storage})
+  Ok(Self{instances:InstanceManager::new(storage.clone()),storage})
  }
  pub fn event(state:LaunchState,message:impl Into<String>)->LaunchEvent{LaunchEvent::new(state,message)}
- pub fn resolve_version<T: DownloadTransport>(&self,resolver:&MojangResolver,version:&str,transport:&T,platform:TargetPlatform)->Result<(Resolution,Vec<LaunchEvent>),EngineError>{
+ pub fn create_instance(&self,id:&str,name:&str,version:&str,loader:Option<String>)->Result<Instance,EngineError>{ self.instances.create(id,name,version,loader) }\n\n pub fn load_instance(&self,id:&str)->Result<crate::instances::InstanceConfig,EngineError>{ self.instances.load(id) }\n\n pub fn list_instances(&self)->Result<Vec<Instance>,EngineError>{ self.instances.list() }\n\n pub fn delete_instance(&self,id:&str)->Result<(),EngineError>{ self.instances.delete(id) }\n\n pub fn resolve_version<T: DownloadTransport>(&self,resolver:&MojangResolver,version:&str,transport:&T,platform:TargetPlatform)->Result<(Resolution,Vec<LaunchEvent>),EngineError>{
   let mut events=vec![Self::event(LaunchState::Resolving,format!("resolving Minecraft {version}"))];
   let metadata_root=&self.storage.cache;
   let resolution=resolver.resolve_with_transport(version,transport,metadata_root,platform)?;
